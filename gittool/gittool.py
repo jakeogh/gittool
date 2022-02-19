18f31fb06b06284546797e721cbe2ed19ef271a8
jakeogh <github.com@v6y.net>
2022-02-18 21:03:12 -0700

    auto-commit

--- gittool/gittool.py
+++ gittool/gittool.py
@@ -3,7 +3,7 @@
 # tab-width:4
 
 import os
-import sys
+# import sys
 from pathlib import Path
 from signal import SIG_DFL
 from signal import SIGPIPE
@@ -18,10 +18,10 @@ from click_auto_help import AHGroup
 from clicktool import click_add_options
 from clicktool import click_global_options
 from clicktool import tv
-#from dulwich import porcelain
+# from dulwich import porcelain
 from dulwich.repo import Repo
 from mptool import output
-from unmp import unmp
+from mptool import unmp
 
 sh.mv = None  # use sh.busybox('mv'), coreutils ignores stdin read errors
 
@@ -29,60 +29,78 @@ signal(SIGPIPE, SIG_DFL)
 
 
 def unstaged_commits_exist(path: Path) -> bool:
-    _git = sh.Command("/home/cfg/git/unstaged_changes_exist_for_file.sh")
-    try:
-        _git(path.as_posix())
-    except sh.ErrorReturnCode_1:
+    result = sh.git.diff_index("HEAD", "--")
+    ic(result.stdout)
+    if path in result:
         return True
     return False
+    # _git = sh.Command("/home/cfg/git/unstaged_changes_exist_for_file.sh")
+    # try:
+    #    _git(path.as_posix())
+    # except sh.ErrorReturnCode_1 as e:
+    #    ic(e)
+    #    return True
+    # return False
 
 
-def get_remotes(repo_path: Path, verbose: Union[bool, int, float],) -> List[dict]:
+def get_remotes(
+    repo_path: Path,
+    verbose: Union[bool, int, float],
+) -> List[dict]:
     repo = Repo(repo_path)
     remotes = []
     for config_file in repo.get_config_stack().backends:
         for k, v in config_file.items():
             if verbose:
-                print(f'{k=}', f'{v=}')
+                print(f"{k=}", f"{v=}")
             if k[0] == b"remote":
                 remotes.append({k[1].decode("utf8"): v[b"url"].decode("utf8")})
     return remotes
 
 
-#@with_plugins(iter_entry_points('click_command_tree'))
+# @with_plugins(iter_entry_points('click_command_tree'))
 @click.group(no_args_is_help=True, cls=AHGroup)
 @click_add_options(click_global_options)
 @click.pass_context
-def cli(ctx,
-        verbose: Union[bool, int, float],
-        verbose_inf: bool,
-        ):
+def cli(
+    ctx,
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+):
 
-    tty, verbose = tv(ctx=ctx,
-                      verbose=verbose,
-                      verbose_inf=verbose_inf,
-                      )
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
 
 
-@cli.command('list-paths')
+@cli.command("list-paths")
 @click.argument("repo_paths", type=str, nargs=-1)
 @click_add_options(click_global_options)
 @click.pass_context
-def list_paths(ctx,
-               repo_paths: tuple[str],
-               verbose: Union[bool, int, float],
-               verbose_inf: bool,
-               ):
+def list_paths(
+    ctx,
+    repo_paths: tuple[str],
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+):
 
-    tty, verbose = tv(ctx=ctx,
-                      verbose=verbose,
-                      verbose_inf=verbose_inf,
-                      )
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
 
     if repo_paths:
         iterator = repo_paths
     else:
-        iterator = unmp(valid_types=[bytes,], verbose=verbose)
+        iterator = unmp(
+            valid_types=[
+                bytes,
+            ],
+            verbose=verbose,
+        )
     del repo_paths
 
     index = 0
@@ -94,34 +112,48 @@ def list_paths(ctx,
             repo_file_path = repo_path / Path(os.fsdecode(thing))
             if verbose:
                 ic(index, repo_path, repo_file_path)
-            #assert _path.exists()  # nope, use .lstat()
-            output(os.fsencode(repo_file_path.as_posix()), tty=tty, verbose=verbose,)
+            # assert _path.exists()  # nope, use .lstat()
+            output(
+                os.fsencode(repo_file_path.as_posix()),
+                tty=tty,
+                verbose=verbose,
+            )
 
 
-@cli.command('list-remotes')
+@cli.command("list-remotes")
 @click.argument("repo_paths", type=str, nargs=-1)
 @click_add_options(click_global_options)
 @click.pass_context
-def list_remotes(ctx,
-                 repo_paths: tuple[str],
-                 verbose: Union[bool, int, float],
-                 verbose_inf: bool,
-                 ):
+def list_remotes(
+    ctx,
+    repo_paths: tuple[str],
+    verbose: Union[bool, int, float],
+    verbose_inf: bool,
+):
 
-    tty, verbose = tv(ctx=ctx,
-                      verbose=verbose,
-                      verbose_inf=verbose_inf,
-                      )
+    tty, verbose = tv(
+        ctx=ctx,
+        verbose=verbose,
+        verbose_inf=verbose_inf,
+    )
 
     if repo_paths:
         iterator = repo_paths
     else:
-        iterator = unmp(valid_types=[bytes,], verbose=verbose)
+        iterator = unmp(
+            valid_types=[
+                bytes,
+            ],
+            verbose=verbose,
+        )
     del repo_paths
 
     index = 0
     for index, _path in enumerate(iterator):
         repo_path = Path(os.fsdecode(_path))
-        remotes = get_remotes(repo_path=repo_path, verbose=verbose,)
+        remotes = get_remotes(
+            repo_path=repo_path,
+            verbose=verbose,
+        )
         for remote in remotes:
             output(remote, tty=tty, verbose=verbose)
