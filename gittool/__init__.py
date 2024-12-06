bc2865bbf3217bf34549d5a461baac5d9927959e
jakeogh <github.com@v6y.net>
2024-12-06 12:53:32 -0700

    auto-commit

--- gittool/__init__.py
+++ gittool/__init__.py
@@ -2,5 +2,5 @@
 isort:skip_file
 """
 
-from .gittool import unstaged_commits_exist as unstaged_commist_exist
+from .gittool import unstaged_commits_exist as unstaged_commits_exist
 from .gittool import get_repo_hashes as get_repo_hashes
